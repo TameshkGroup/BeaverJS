bb3511051bce74842839b353199cb31126370cc9
Mohammadreza Khani <mohammadreza.khani.71@gmail.com>
2021-12-12 01:21:16 +0330

    vite.config.js cleaned

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,5 @@ import { defineConfig } from 'vite'
 import phenomenJSX from './vite-phenomen-jsx'
 
 export default defineConfig({
-    build: {
-        /* rollupOptions: {
-            plugins: [jsx( {factory: 'Mohamad.createElement'} )]
-        } */
-    },
     plugins: [phenomenJSX()],
 })
