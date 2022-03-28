67f0f29eb36eb7e0b796668eb8c8f74848a79feb
Mohammadreza Khani <mohammadreza.khani.71@gmail.com>
2022-03-29 01:40:08 +0430

    vite plugin exported

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,13 @@
 import { defineConfig } from 'vite'
 import viteBeaverPlugin from './vite-beaver-plugin'
-import path from 'path';
+import path from 'path'
 
 export default defineConfig({
     build: {
         lib: {
             entry: path.resolve(__dirname, 'src/index.ts'),
             name: 'BeaverJS',
-            fileName: (format) => `beaver.${format}.js`
+            fileName: (format) => `beaver.${format}.js`,
         },
         rollupOptions: {
             // make sure to externalize deps that shouldn't be bundled
@@ -17,10 +17,10 @@ export default defineConfig({
                 // Provide global variables to use in the UMD build
                 // for externalized deps
                 globals: {
-                    vue: 'Vue'
-                }
-            }
-        }
+                    vue: 'Vue',
+                },
+            },
+        },
     },
     plugins: [
         viteBeaverPlugin(),
